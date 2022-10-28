4bd19fae50f05cb1ec6e559210ee6e059046f980
김영민 <rpan0522@gmail.com>
2022-10-28 23:07:19 +0900

    실습일지

---
+++ test/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ProfileCard from './chapter11/ProfileCard';
+
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+        <ProfileCard/>
+);
